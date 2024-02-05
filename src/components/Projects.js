67abf083b31b25be9a53a30b1975aa4f471c0217
Almog Levi <almoglevi71@gmail.com>
2024-02-06 01:16:50 +0200

    Updating project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useCallback, useState } from "react";
 import "../styles.css"
-import {Button,Container, Image,Row,Col} from "react-bootstrap";
+import { Button, Container, Image, Row, Col } from "react-bootstrap";
 import CostManagerImage from "../images/CostManagerImage.png";
 import ReactWeatherAppImage from "../images/ReactWeatherAppImage.png";
 import PopularMoviePredictionImage from "../images/PopularMoviePredictionImage.png";
@@ -10,59 +10,75 @@ import simonGameImage from "../images/simonGameImage.png";
 import quizzyImage from "../images/quizzyImage.png";
 import IotSmartLockImage from "../images/IotSmartLockImage.png";
 import Carousel from 'react-bootstrap/Carousel';
+import ecommerceImg from "../images/ecommerceImg.png";
+import ecommerceAdminImg from "../images/ecommerceAdminImg.png";
 
 const cardItems = [
     {
         id: 1,
+        headline: 'Nike-Shoes Ecommerce',
+        photo: ecommerceImg,
+        description: "A Nike shoes E-commerce store built with Next.js, React.js and Tailwind CSS. Features include product listing, shopping cart functionality and order processing with Stripe.",
+        url: 'https://nike-shoes-ecommerce1.vercel.app/'
+    },
+    {
+        id: 2,
+        headline: 'Nike-Shoes Ecommerce Admin',
+        photo: ecommerceAdminImg,
+        description: 'A Next.js project for a Nike shoes E-commerce admin panel, providing a user interface for managing products, orders and categories.',
+        url: 'https://github.com/Almoglevi1/Nike-Shoes-Ecommerce-Admin'
+    },
+    {
+        id: 3,
         headline: 'Cost Manager React web application',
         photo: CostManagerImage,
         description: "This app helps manage the user's expenses. Enables adding, editing, deleting, displaying the data, sorting and filtering.",
         url: 'https://almoglevi1.github.io/Cost-Manager-App/'
     },
     {
-        id: 2,
+        id: 4,
         headline: 'Weather App',
         photo: ReactWeatherAppImage,
         description: 'This app was developed using React while using OpenWeather API and GeoDB cities API.',
         url: 'https://almoglevi1.github.io/Weather-App/'
     },
     {
-        id: 3,
+        id: 5,
         headline: 'Quizzy',
         photo: quizzyImage,
         description: 'Quizzy Library in Java while using classic design patterns: Singleton, Factory, Prototype, Builder and Data Access Object.',
         url: 'https://github.com/Almoglevi1/Quizzy'
     },
     {
-        id: 4,
+        id: 6,
         headline: 'Popular Movies Prediction',
         photo: PopularMoviePredictionImage,
         description: 'Data science - machine learning project in python.',
         url: 'https://github.com/Almoglevi1/Popular-Movies-Prediction'
     },
     {
-        id: 5,
+        id: 7,
         headline: 'Paint',
         photo: PaintImage,
         description: 'Paint application was developed using C++ and MFC framework.',
         url: 'https://github.com/Almoglevi1/Paint'
     },
     {
-        id: 6,
+        id: 8,
         headline: 'Keeper App',
         photo: keeperAppImage,
         description: 'This app using React and Material-UI. Store ideas and notes in a structured way in the form of React components. Create a number of notes and can delete them too.',
         url: 'https://almoglevi1.github.io/Keeper-App/'
     },
     {
-        id: 7,
+        id: 9,
         headline: 'Simon Game',
         photo: simonGameImage,
         description: 'This game was developed using HTML, CSS and JavaScript.',
         url: 'https://almoglevi1.github.io/Simon-Game/'
     },
     {
-        id: 8,
+        id: 10,
         headline: 'IOT Smart-Lock',
         photo: IotSmartLockImage,
         description: 'IOT Smart Lock application developed using python.',
@@ -131,29 +147,29 @@ const Projects = () => {
     return (
         <section id="Projects">
             <h3 className="sectionHeader">My Projects</h3>
-    
-                <Carousel className="carousel slide card" variant="dark">
-                        {cardItems.map((card, index) => (
-                            <Carousel.Item interval={2000}>
-                                <Row>
-                                    <Col lg={1} md={1}>
-                                    </Col>
-                                    <Col className="projectDetails" lg={3} md={3} xs={{ span:"auto", order: 1 }} >
-                                        <h2>{card.headline}</h2>
-                                        <p>{card.description}</p>
-                                        <Button className="viewProjectBtn" href={card.url} target={"_blank"} variant="primary">
-                                            View Project
-                                        </Button>
-                                    </Col>
-                                    <Col lg={7} md={7} xs={{ span: "auto", order: 2}}>
-                                        <Image className="project-image" src={card.photo} alt={card.headline}/>
-                                    </Col>
-                                </Row>
-                                <Col lg={1} md={1}>
-                                </Col>
-                            </Carousel.Item>
-                        ))}
-                </Carousel>
+
+            <Carousel className="carousel slide card" variant="dark">
+                {cardItems.map((card, index) => (
+                    <Carousel.Item interval={2000}>
+                        <Row>
+                            <Col lg={1} md={1}>
+                            </Col>
+                            <Col className="projectDetails" lg={3} md={3} xs={{ span: "auto", order: 1 }} >
+                                <h2>{card.headline}</h2>
+                                <p>{card.description}</p>
+                                <Button className="viewProjectBtn" href={card.url} target={"_blank"} variant="primary">
+                                    View Project
+                                </Button>
+                            </Col>
+                            <Col lg={7} md={7} xs={{ span: "auto", order: 2 }}>
+                                <Image className="project-image" src={card.photo} alt={card.headline} />
+                            </Col>
+                        </Row>
+                        <Col lg={1} md={1}>
+                        </Col>
+                    </Carousel.Item>
+                ))}
+            </Carousel>
 
         </section>
     );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -15,100 +15,116 @@ import mongoDBIcon from '../images/mongoDBIcon.png';
 import mySQLIcon from '../images/mySQLIcon.png';
 import bootstrapIcon from '../images/bootstrapIcon.png';
 import jqueryIcon from '../images/jqueryIcon.png';
+import TailwindCSSIcon from '../images/TailwindCSSIcon.png';
+import nextJsIcon from '../images/nextJsIcon.png';
+
 const Skills = () => {
 
   return (
-      <section id="skills">
+    <section id="skills">
       <Container className="my-5">
         <h3 className="skillsTitle">Skills</h3>
-        <br/>
-        <br/>
-        <Table  borderless className="Skills">
+        <br />
+        <br />
+        <Table borderless className="Skills">
           <tbody>
 
-          {/*first row*/}
-          <tr>
-            <td className="text-center">
-              <Image src={javaIcon} alt="Java" className="skillsImageSize"/>
-              <p>Java</p>
-            </td>
-            <td className="text-center">
-              <Image src={pythonIcon} alt="Python" className="skillsImageSize"/>
-              <p>Python</p>
-            </td>
-            <td className="text-center">
-              <Image src={cppIcon} alt="C++" className="skillsImageSize"/>
-              <p>C++</p>
-            </td>
-          </tr>
+            {/*first row*/}
+            <tr>
+              <td className="text-center">
+                <Image src={javaIcon} alt="Java" className="skillsImageSize" />
+                <p>Java</p>
+              </td>
+              <td className="text-center">
+                <Image src={pythonIcon} alt="Python" className="skillsImageSize" />
+                <p>Python</p>
+              </td>
+              <td className="text-center">
+                <Image src={cppIcon} alt="C++" className="skillsImageSize" />
+                <p>C++</p>
+              </td>
+            </tr>
 
-          {/*second row*/}
-          <tr>
-            <td className="text-center">
-              <Image src={cIcon} alt="C" className="skillsImageSize"/>
-              <p>C</p>
-            </td>
-            <td className="text-center">
-              <Image src={htmlIcon} alt="HTML" className="skillsImageSize"/>
-              <p>HTML</p>
-            </td>
-            <td className="text-center">
-              <Image src={cssIcon} alt="CSS" className="skillsImageSize"/>
-              <p>CSS</p>
-            </td>
-          </tr>
+            {/*second row*/}
+            <tr>
+              <td className="text-center">
+                <Image src={cIcon} alt="C" className="skillsImageSize" />
+                <p>C</p>
+              </td>
+              <td className="text-center">
+                <Image src={htmlIcon} alt="HTML" className="skillsImageSize" />
+                <p>HTML</p>
+              </td>
+              <td className="text-center">
+                <Image src={cssIcon} alt="CSS" className="skillsImageSize" />
+                <p>CSS</p>
+              </td>
+            </tr>
 
-          {/*third row*/}
-          <tr>
-            <td className="text-center">
-              <Image src={jsIcon} alt="JavaScript" className="skillsImageSize"/>
-              <p>JavaScript</p>
-            </td>
-            <td className="text-center">
-              <Image src={jqueryIcon} alt="jQuery" className="skillsImageSize"/>
-              <p>jQuery</p>
-            </td>
-            <td className="text-center">
-              <Image src={bootstrapIcon} alt="Bootstrap" className="bootstrapImageSize"/>
-              <p>Bootstrap</p>
-            </td>
+            {/*third row*/}
+            <tr>
+              <td className="text-center">
+                <Image src={jsIcon} alt="JavaScript" className="skillsImageSize" />
+                <p>JavaScript</p>
+              </td>
+              <td className="text-center">
+                <Image src={jqueryIcon} alt="jQuery" className="skillsImageSize" />
+                <p>jQuery</p>
+              </td>
+              <td className="text-center">
+                <Image src={bootstrapIcon} alt="Bootstrap" className="bootstrapImageSize" />
+                <p>Bootstrap</p>
+              </td>
             </tr>
 
             {/*fourth row*/}
             <tr>
               <td className="text-center">
-                <Image src={reactIcon} alt="React" className="skillsImageSize"/>
-                <p>React</p>
+                <Image src={TailwindCSSIcon} alt="TailwindCSS" className="skillsImageSize" />
+                <p>Tailwind CSS</p>
               </td>
-            <td className="text-center">
-              <Image src={nodeJSIcon} alt="NodeJs" className="skillsImageSize"/>
-              <p>NodeJs</p>
-            </td>
-            <td className="text-center">
-              <Image src={mongoDBIcon} alt="MongoDB" className="skillsImageSize"/>
-              <p>MongoDB</p>
-            </td>
-          </tr>
+              <td className="text-center">
+                <Image src={reactIcon} alt="ReactJs" className="skillsImageSize" />
+                <p>React.js</p>
+              </td>
+              <td className="text-center">
+                <Image src={nextJsIcon} alt="NextJs" className="skillsImageSize" />
+                <p>Next.js</p>
+              </td>
+            </tr>
 
-          {/*fifth row*/}
-          <tr>
-            <td className="text-center">
-              <Image src={mySQLIcon} alt="MySQL" className="skillsImageSize"/>
-              <p>MySQL</p>
-            </td>
-            <td className="text-center">
-              <Image src={gitIcon} alt="Git" className="skillsImageSize"/>
-              <p>Git</p>
-            </td>
-            <td className="text-center">
-              <Image src={gitHubIcon} alt="GitHub" className="skillsImageSize"/>
-              <p>GitHub</p>
-            </td>
-          </tr>
+            {/*fifth row*/}
+            <tr>
+              <td className="text-center">
+                <Image src={nodeJSIcon} alt="NodeJs" className="skillsImageSize" />
+                <p>Node.js</p>
+              </td>
+              <td className="text-center">
+                <Image src={mongoDBIcon} alt="MongoDB" className="skillsImageSize" />
+                <p>MongoDB</p>
+              </td>
+              <td className="text-center">
+                <Image src={mySQLIcon} alt="MySQL" className="skillsImageSize" />
+                <p>MySQL</p>
+              </td>
+            </tr>
+
+            {/*sixth row*/}
+            <tr>
+
+              <td className="text-center">
+                <Image src={gitIcon} alt="Git" className="skillsImageSize" />
+                <p>Git</p>
+              </td>
+              <td className="text-center">
+                <Image src={gitHubIcon} alt="GitHub" className="skillsImageSize" />
+                <p>GitHub</p>
+              </td>
+            </tr>
           </tbody>
         </Table>
       </Container>
-      </section>
+    </section>
   );
 };
 
